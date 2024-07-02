d104ef502c704e14009d1bda9c5c6bf1759d2eb2
Filippos Kolliniatis <it2021043@hua.gr>
2024-07-02 18:26:26 +0300

    css updates

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,3 +1,11 @@
 <template>
     <footer><!-- empty footer :) --></footer>
 </template>
+
+<style>
+.container {
+  background-color: #2d2a2a;
+  color : #ffffff;
+}
+
+</style>
--- src/views/CreateUserView.vue
+++ src/views/CreateUserView.vue
@@ -4,18 +4,18 @@ const router = useRouter();
 </script>
 
 <template>
-  <div class="bg-body-tertiary">
+  <div class = " center-container">
     <div class="row py-4 px-3">
         <div class="mb-4">
   <h1> Select if you want to create a <b>Citizen</b> or a <b>Secretary</b></h1>
-  <div class = " center-container">
+
   <td><button @click="router.push({ name: 'createCitizen' })" class="btn btn-primary btn-lg">
     Create Citizen
   </button></td>
   <td><button @click="router.push({ name: 'createSecretary' })" class="btn btn-primary btn-lg">
     Create Secretary
   </button></td>
-         </div>
+
         </div>
       </div>
 
@@ -24,15 +24,22 @@ const router = useRouter();
 
 
 <style scoped>
+
+
 .center-container {
+  color : #9a7c7c;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
   height: 20vh; /* Adjust as needed to center vertically */
+
 }
+
+
 
 .btn {
   margin-right: 40px; /* Adjust the margin for horizontal spacing */
+  background-color : #484343;
 }
 </style>
